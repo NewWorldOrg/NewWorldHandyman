fda229964e285d42c68c7e96ae9fa31343e000e0
fyui001 <yun.cod@gmail.com>
2020-04-30 02:18:16 +0900

    icon周りの整備

--- cog/MyCog.py
+++ cog/MyCog.py
@@ -10,23 +10,37 @@ from src.MyModules import MyModules as myMod
 class MyCog(commands.Cog):
 
     def __init__(self, bot):
-        self.bot = bot
         base = os.path.dirname(os.path.abspath(__file__))
         conf_path = os.path.normpath(os.path.join(base, '../'))
         conf = configparser.ConfigParser()
         conf.read(conf_path+'/config.ini', encoding='utf-8')
         self.general_text_channel_id = int(conf['NEW_WORLD']['GENERAL_TEXT_CHANNEL_ID'])
+        self.bot = bot
+        self.bot_id = int(conf['DEFAULT']['BOT_ID'])
+        self.icon_url = 'https://cdn.discordapp.com/avatars/{id}/{avatar}.png'
         self.sleep_alert.start()
 
     @commands.command(name='のんだ')
     async def drug(self, ctx, arg):
+
         mod = myMod()
         user = ctx.author.name
+        bot  = self.bot.get_user(self.bot_id)
         embed_description = f"{user} took '{arg}' at {datetime.now().strftime('%H:%M')}"
-        icon = f'https://cdn.discordapp.com/avatars/{str(ctx.author.id)}/{ctx.author.avatar}.png'
+
+        bot_icon = self.icon_url.format(
+            id = str(self.bot_id),
+            avatart = bot.avatar,
+        )
+
+        icon = self.icon_url.format(
+            id = str(ctx.author.id),
+            avatar = ctx.author.avatar,
+        )
+
         embed=discord.Embed(title='のんだ', description=embed_description, color=0xff4dd8)
         embed.set_author(name=user, icon_url=icon)
-        embed.set_thumbnail(url="https://cloud.mogamin.net/apps/files_sharing/publicpreview/Q56wtgd8x2SEoXk?fileId=299&file=%2FEQ9n5UUUEAAYwTR.jpeg&x=1680&y=1050&a=true")
+        embed.set_thumbnail(url=bot_icon)
         embed.set_footer(text='NewWorldHandyman')
         if not mod.save_use_drug_history(user, arg):
             embed.add_field(name="", value="薬物の検出に失敗しました", inline=True)
