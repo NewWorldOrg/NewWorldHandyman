b1856d0a3725d73f0cb04cca5a3b1cd2b81b2631
fyui001 <yun.cod@gmail.com>
2020-11-16 18:38:39 +0900

    Intentsの有効化

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
 class MyBot(commands.Bot):
 
-    def __init__(self, command_prefix):
-        super().__init__(command_prefix)
+    def __init__(self, command_prefix, intents):
+        super().__init__(command_prefix, intents=intents)
         cog_list = [
             'cog.MyCog'
         ]
@@ -22,8 +23,10 @@ def main():
     base_path = os.path.dirname(os.path.abspath(__file__))
     dotenv_path = os.path.join(base_path, '.env')
     load_dotenv(dotenv_path)
+    intents = discord.Intents.all()
 
-    client = MyBot(command_prefix='$')
+
+    client = MyBot(command_prefix='$', intents=intents)
     client.run(os.getenv('BOT_TOKEN'))
 
 if __name__ == '__main__':
