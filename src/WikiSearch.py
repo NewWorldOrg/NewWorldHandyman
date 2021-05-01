b33fb7cefb230b0a8c048b5f1f912ee7cc01a493
fyui001 <yun.cod@gmail.com>
2021-05-02 00:39:41 +0900

    サジェッションとリダイレクトを無効化した

--- src/WikiSearch.py
+++ src/WikiSearch.py
@@ -4,7 +4,7 @@ import wikipedia
 def wikipediaSearch(search_text: str):
     wikipedia.set_lang('ja')
     try:
-        url = wikipedia.page(search_text).url
+        url = wikipedia.page(title=search_text, auto_suggest=False, redirect=False).url
         return url
     except:
         return False
